81082ec2e7750e88702694ae9048910e28787379
Subrat Kumar Rout <subratrout986@gmail.com>
2026-01-15 04:15:50 +0530

    build a simple ui

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-// https://api.github.com/users?since=${ran}&per_page=${count}
+import  React,{useState,useEffect} from "react";
+import ReactDOM from "react-dom/client"
+import Header from "./component/header.js";
+ import Body from "./component/body.js";
+
+function Main(){
+    return(
+      <>
+      <Header></Header>
+      <Body></Body>
+      </>
+    )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Main/>)
---
+++ component/header.js
@@ -0,0 +1,12 @@
+import  {useState,useEffect, Component} from "react";
+
+function Header(){
+    return(
+        <div className="head">
+         <h1>Git Hub Profil's</h1>
+        </div>
+       
+    )
+}
+
+export default Header;
